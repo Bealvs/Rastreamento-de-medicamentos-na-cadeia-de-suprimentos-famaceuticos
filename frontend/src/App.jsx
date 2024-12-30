cd7656a9e47853434e8ff02f8c38241bea2004b4
Rakel Torres Carvalho <135343610+rakeltorres@users.noreply.github.com>
2024-12-30 12:25:12 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/tracking-user" element={<TrackingUser />} />
         <Route path="/result-user" element={<ResultUser />} />
