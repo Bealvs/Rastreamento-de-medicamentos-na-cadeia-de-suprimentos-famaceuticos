c8f9e7108ef7ac263ac61455a299cece72020360
FelipeFerraz4 <felipeferraz2402@gmail.com>
2025-01-04 15:43:21 -0300

    feat: update routes

--- backend/src/routes/trackingRoutes.js
+++ backend/src/routes/trackingRoutes.js
@@ -1,12 +1,16 @@
 import express from "express";
 import trackingController from "../controllers/trackingController.js";
-import authMiddleware from "../middlewares/authMiddleware.js"; // Import authentication middleware
+import authMiddleware from "../middlewares/authMiddleware.js"; // Importando middleware de autenticação
 
 const router = express.Router();
 
 // Routes for Tracking
-router.post("/:productId", authMiddleware, trackingController.createTracking);
-router.get("/", trackingController.getAllTrackings);
-router.get("/:trackingId", trackingController.getTrackingById);
+router.post(
+  "/tracking/:trackingCode",
+  authMiddleware,
+  trackingController.createTracking
+); // Criar uma nova entrada de tracking
+router.get("/", trackingController.getAllTrackings); // Buscar todos os rastreamentos
+router.get("/:trackingId", trackingController.getTrackingById); // Buscar rastreamento pelo ID
 
 export default router;
