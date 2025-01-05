d5883671359e81c3f6e506ad8f769067e0dd1067
FelipeFerraz4 <felipeferraz2402@gmail.com>
2025-01-05 14:46:56 -0300

    feat: update blockchain config with medication tracking contract

--- backend/src/config/blockchain.js
+++ backend/src/config/blockchain.js
@@ -1,12 +1,14 @@
 import Web3 from "web3";
 import helloWorldJSON from "../../../blockchain/build/contracts/HelloWorld.json" assert { type: "json" };
+import MedicationTrackingJSON from "../../../blockchain/build/contracts/MedicationTracking.json" assert { type: "json" };
 import dotenv from "dotenv";
 
 dotenv.config({
   path: process.env.NODE_ENV === "production" ? ".env.production" : ".env",
 });
 
-const { GANACHE_URL, HELLOWORLD_ADDRESS } = process.env;
+const { GANACHE_URL, HELLOWORLD_ADDRESS, MEDICATIONTRACKING_ADDRESS } =
+  process.env;
 
 if (!GANACHE_URL) {
   throw new Error(
@@ -18,18 +20,18 @@ if (!GANACHE_URL) {
 const web3 = new Web3(GANACHE_URL);
 
 // Função para obter o endereço do contrato dinamicamente em desenvolvimento
-const getContractAddress = async () => {
+const getContractAddress = async (contractJSON, envAddress) => {
   if (process.env.NODE_ENV === "production") {
-    if (!HELLOWORLD_ADDRESS) {
+    if (!envAddress) {
       throw new Error(
-        "Por favor, configure a variável HELLOWORLD_ADDRESS no arquivo .env.production"
+        "Por favor, configure a variável de endereço do contrato no arquivo .env.production"
       );
     }
-    return HELLOWORLD_ADDRESS;
+    return envAddress;
   } else {
     // Em desenvolvimento, obtenha o endereço dinamicamente
     const networkId = await web3.eth.net.getId();
-    const deployedNetwork = helloWorldJSON.networks[networkId];
+    const deployedNetwork = contractJSON.networks[networkId];
 
     if (!deployedNetwork) {
       throw new Error("Contrato não implantado na rede especificada.");
@@ -39,25 +41,34 @@ const getContractAddress = async () => {
   }
 };
 
-// Função para criar uma instância do contrato
-const createContractInstance = async () => {
+// Função para criar uma instância de um contrato
+const createContractInstance = async (contractJSON, envAddress) => {
   try {
-    const contractAddress = await getContractAddress();
-    const helloWorldABI = helloWorldJSON.abi;
+    const contractAddress = await getContractAddress(contractJSON, envAddress);
+    const contractABI = contractJSON.abi;
 
-    const helloWorldcontractInstance = new web3.eth.Contract(
-      helloWorldABI,
+    const contractInstance = new web3.eth.Contract(
+      contractABI,
       contractAddress
     );
 
     console.log(`Contrato configurado no endereço: ${contractAddress}`);
-    return helloWorldcontractInstance;
+    return contractInstance;
   } catch (error) {
     console.error("Erro ao criar a instância do contrato:", error);
     throw error;
   }
 };
 
-const helloWorldcontractInstance = await createContractInstance();
+// Criar instâncias dos contratos
+const helloWorldContractInstance = await createContractInstance(
+  helloWorldJSON,
+  HELLOWORLD_ADDRESS
+);
 
-export { web3, helloWorldcontractInstance };
+const medicationTrackingContractInstance = await createContractInstance(
+  MedicationTrackingJSON,
+  MEDICATIONTRACKING_ADDRESS
+);
+
+export { web3, helloWorldContractInstance, medicationTrackingContractInstance };
